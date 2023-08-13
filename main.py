a7de54af5b4c1cbd5a15ec55f2f3d79fc6d518df
d43v30n <anjey.mail@gmail.com>
2023-08-13 18:33:40 +0200

    correct normalizer: now returns normalized ext

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import normalize
 import json
 
 
-def classify(file_name):
+def classify(file_name, file='extensions.json'):
     '''Return file classification
 
-    extension -- str input filename
-    type -- str output classification of file
+    file_name -- str input filename
+    file -- json with file types and extensions
 
+    returns str output classification of file
     returns None if extension is unknown
     '''
-    file = 'extensions.json'
     with open(file, "r") as f:
         types = json.loads(f.read())
     for type, extensions in types.items():
@@ -29,15 +29,15 @@ def parse_folder(path):
     '''
     for item in path.iterdir():
         if item.is_dir():
-            print("Folder found:".ljust(25), item)
+            print("Folder found:".ljust(15), item)
             parse_folder(item)
         else:
-            print("File found:".ljust(25),  item)
-            print("Classified as".ljust(25), classify(item))
+            print("File found:".ljust(15),  item)
+            print("Classified as:".ljust(15), classify(item))
 
 
 if __name__ == '__main__':
     for p in sys.argv[1:]:
         path = Path(p)
-        print("Starting to sort folder:".ljust(25), path)
+        print("Starting to sort folder:", path)
         parse_folder(path)
