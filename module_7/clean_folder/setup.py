f16df3f5f0612fc59a306d49e4aeda44361dad5c
d43v30n <anjey.mail@gmail.com>
2023-09-18 19:40:25 +0200

    homework

---
+++ module_7/clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='0.1',
+    description='Module 7 Homework',
+    url='https://github.com/d43v30n/goit-python/tree/main/module_7',
+    author='Andrii',
+    author_email='andrii@example.com',
+    license='MIT',
+    packages=[sys, pathlib, normalize, json, shutil]
+)
