828976c7ebdbeaac374f2408ab29877b5aca39ea
bubliq portable <anjey.mail@gmail.com>
2023-09-21 11:32:32 +0200

    homework

---
+++ module_10/main.py
@@ -0,0 +1,87 @@
+import re
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+    
+    def add_phone(self, phone):
+        phone = re.sub(r'[^0-9]', '', phone)
+        if len(phone) == 10:
+            self.phones.append(phone)
+        if len(phone) == 8:
+            self.phones.append(f"38{phone}")
+
+
+
+class AddressBook(UserDict):
+    
+    def __init__(self):
+        self.data = {}
+    
+    def add_record(self, record):
+        self.data.update({record.name.value: record})
+
+    def find(self):
+        ...
+
+    def delete(self):
+        ...
+
+
+
+
+# --------------
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
---
+++ module_10/readme.md
@@ -0,0 +1,2 @@
+### Homework for Module 9
+https://github.com/d43v30n/goit-python/tree/main/module_9
