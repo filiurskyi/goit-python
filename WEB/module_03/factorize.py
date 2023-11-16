7f3c183fcf5340a9ea021fa43673a6cc73b41765
d43v30n <anjey.mail@gmail.com>
2023-11-16 12:01:11 +0100

    .

--- WEB/module_03/factorize.py
+++ WEB/module_03/factorize.py
@@ -27,7 +27,7 @@ def factorize(number):
     for i in range(1, number + 1):
         if number % i == 0:
             result.append(i)
-    logger.debug(f"{result=}")
+    logger.info(f"{result=}")
     return result
 
 
@@ -35,58 +35,59 @@ def normal_factorize(*numbers):
     start_time = time()
     logger.info("running normal_factorize")
     result = map(factorize, numbers)
-    func_run_time = time() - start_time
+    end_time = time()
+    func_run_time = end_time - start_time
     logger.info(f"normal func run time is : {func_run_time}")
     return result
 
 
 def multiprocess_factorize(*numbers):
-    start_time = time()
     logger.info("running multiprocess_factorize")
+    start_time = time()
     cpus = cpu_count()
     with Pool(processes=cpus) as pool:
         result = pool.map(factorize, numbers)
-
-    func_run_time = time() - start_time
+    end_time = time()
+    func_run_time = end_time - start_time
     logger.info(f"multiprozess func run time is : {func_run_time}")
     return result
 
 
 if __name__ == "__main__":
-    a, b, c, d = normal_factorize(128, 255, 99999, 10651060)
-    e, f, g, h = multiprocess_factorize(128, 255, 99999, 10651060)
+    a, b, c, d = normal_factorize(1280000, 2550000, 9999900, 106510600)
+    e, f, g, h = multiprocess_factorize(1280000, 2550000, 9999900, 106510600)
 
-    assert a == e
-    assert b == f
-    assert c == g
-    assert d == h
+    # assert a == e
+    # assert b == f
+    # assert c == g
+    # assert d == h
 
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [
-        1,
-        2,
-        4,
-        5,
-        7,
-        10,
-        14,
-        20,
-        28,
-        35,
-        70,
-        140,
-        76079,
-        152158,
-        304316,
-        380395,
-        532553,
-        760790,
-        1065106,
-        1521580,
-        2130212,
-        2662765,
-        5325530,
-        10651060,
-    ]
+    # assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    # assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    # assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    # assert d == [
+    #     1,
+    #     2,
+    #     4,
+    #     5,
+    #     7,
+    #     10,
+    #     14,
+    #     20,
+    #     28,
+    #     35,
+    #     70,
+    #     140,
+    #     76079,
+    #     152158,
+    #     304316,
+    #     380395,
+    #     532553,
+    #     760790,
+    #     1065106,
+    #     1521580,
+    #     2130212,
+    #     2662765,
+    #     5325530,
+    #     10651060,
+    # ]
