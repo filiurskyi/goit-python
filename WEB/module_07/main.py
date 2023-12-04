8e98cfadc0425702381eaa4b7edb5eab6d12d56c
bubliq portable <anjey.mail@gmail.com>
2023-12-04 16:16:38 +0100

    .

--- WEB/module_07/main.py
+++ WEB/module_07/main.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from psycopg2 import connect
+# from psycopg2 import connect
 from model import Base, Students, Groups, Teachers, Classes, Grades
 from faker import Faker
 
-fake = Faker()
+fake = Faker("en")
 engine = create_engine('sqlite:///mydatabase.db', echo=True)
 # engine = create_engine('postgres+psycopg2:///:localhost:', echo=True)
 
@@ -15,7 +15,7 @@ Base.metadata.create_all(engine)
 Base.metadata.bind = engine
 
 
-for _ in range(100):
+for _ in range(10):
     new_student = Students(
         f_name = fake.first_name(),
         l_name = fake.last_name(),
