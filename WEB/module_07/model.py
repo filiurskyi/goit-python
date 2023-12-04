5ea286554008f58123e58e3f1beafb2907c6a76c
bubliq portable <anjey.mail@gmail.com>
2023-12-04 16:03:30 +0100

    .

--- WEB/module_07/model.py
+++ WEB/module_07/model.py
@@ -1,9 +1,36 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, SmallInteger
 
 Base = declarative_base()
 
 
 class Students(Base):
-    __tablename__ = "lalala"
-    self.
-    
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    f_name = Column(String(250), nullable=False)
+    l_name = Column(String(250), nullable=False)
+    stud_group = Column(SmallInteger)
+
+
+class Groups(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Teachers(Base):
+    __tablename__ = 'teachers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Classes(Base):
+    __tablename__ = 'classes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Grades(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
