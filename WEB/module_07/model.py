f1dd3907bac88725168c5917e545719b45fc89a0
d43v30n <anjey.mail@gmail.com>
2023-12-04 22:02:45 +0100

    .

--- WEB/module_07/model.py
+++ WEB/module_07/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String, DateTime
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -9,7 +9,22 @@ class Students(Base):
     id = Column(Integer, primary_key=True)
     f_name = Column(String(250), nullable=False)
     l_name = Column(String(250), nullable=False)
-    stud_group = Column(SmallInteger)
+    stud_group = Column(SmallInteger, ForeignKey("groups.id"), nullable=False)
+
+
+class Teachers(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True)
+    f_name = Column(String(250), nullable=False)
+    l_name = Column(String(250), nullable=False)
+    classes = relationship("Classes")
+
+
+class Classes(Base):
+    __tablename__ = "classes"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    teachers = Column(SmallInteger, ForeignKey("teachers.id"))
 
 
 class Groups(Base):
@@ -18,22 +33,10 @@ class Groups(Base):
     name = Column(String(250), nullable=False)
 
 
-class Teachers(Base):
-    __tablename__ = "teachers"
-    id = Column(Integer, primary_key=True)
-    f_name = Column(String(250), nullable=False)
-    l_name = Column(String(250), nullable=False)
-    classes = relationship()
-
-
-class Classes(Base):
-    __tablename__ = "classes"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    teachers = relationship("Teachers")
-
-
 class Grades(Base):
     __tablename__ = "grades"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    grade = Column(Integer)
+    class_id = Column(SmallInteger, ForeignKey("classes.id"))
+    teacher_id = Column(SmallInteger, ForeignKey("teachers.id"))
+    date = Column(DateTime)
--- WEB/module_07/seed.py
+++ WEB/module_07/seed.py
@@ -1,4 +1,5 @@
-from random import randint
+import datetime
+from random import randint, choice
 
 from faker import Faker
 from sqlalchemy import create_engine
@@ -19,12 +20,80 @@ session = DBSession()
 Base.metadata.create_all(engine)
 Base.metadata.bind = engine
 
-if __name__ == "__main__":
-    for _ in range(10):
+CLASSES_NAMES = [
+    "Astronomy",
+    "Physics",
+    "Maths",
+    "Computer Science",
+    "Programming",
+    "Philosopy",
+    "Biology",
+]
+
+GRADES_NAMES = [5, 4, 3, 2, 1]
+
+
+def generate_students(amount):
+    for _ in range(amount):
+        grps = session.query(Groups.id).all()
+        print("-------------------", grps)
         new_student = Students(
             f_name=fake.first_name(),
             l_name=fake.last_name(),
-            stud_group=randint(1, 3),
+            stud_group=grps
         )
         session.add(new_student)
         session.commit()
+        session.close()
+
+
+def generate_teachers(amount):
+    for _ in range(amount):
+        new_teacher = Teachers(
+            f_name=fake.first_name(),
+            l_name=fake.last_name(),
+            # classes=session.query(Classes).group_by("id"),
+        )
+        session.add(new_teacher)
+        session.commit()
+        session.close()
+
+
+def generate_classes():
+    for cls in CLASSES_NAMES:
+        new_class = Classes(
+            name=cls,
+        )
+        session.add(new_class)
+        session.commit()
+        session.close()
+
+
+def generate_groups(amount):
+    for _ in range(amount):
+        new_group = Groups(
+            name=fake.country(),
+        )
+        session.add(new_group)
+        session.commit()
+        session.close()
+
+
+def generate_grades(amount_per_student):
+    for _ in range(amount_per_student):
+        new_grade = Grades(
+            grade=choice(GRADES_NAMES),
+            date=datetime.datetime.now(),
+        )
+        session.add(new_grade)
+        session.commit()
+        session.close()
+
+
+if __name__ == "__main__":
+    generate_groups(5)
+    generate_classes()
+    generate_students(50)
+    generate_teachers(10)
+    generate_grades(10)
+
