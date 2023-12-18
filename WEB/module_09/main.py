53e467cef98fbc80c7aa700f1befdcb3aa25ed28
d43v30n <anjey.mail@gmail.com>
2023-12-18 22:54:20 +0100

    .

--- WEB/module_09/main.py
+++ WEB/module_09/main.py
@@ -1,28 +1,12 @@
 import requests
-import bs4 as bs
-from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
-
-
-import http.client
-base_url = "index.minfin.com.ua"
-url_path = "/ua/russian-invading/casualties/3/"
-full_url = "https://index.minfin.com.ua/ua/russian-invading/casualties/3/"
-
-
-def http_get():
-    conn = http.client.HTTPSConnection(base_url, port=443)
-    conn.request("GET", url_path)
-    res = conn.getresponse()
-    data = res.read().decode("utf-8")
-    conn.close()
-    return data
-
-
-def get_url(url):
-    response = requests.get(url)
-    return response
+# from urllib.parse import urljoin, urlparse
 
 
 if __name__ == '__main__':
-    print(http_get() == get_url(full_url).text)
+    url = 'https://quotes.toscrape.com/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('span', class_='text')
+    print(quotes)
