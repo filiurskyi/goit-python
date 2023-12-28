40b9110a4f090d87a3457ec3f5e83828315d7436
d43v30n <anjey.mail@gmail.com>
2023-12-29 00:31:36 +0100

    login, logout, access ok

--- WEB/module_10/backend/users/forms.py
+++ WEB/module_10/backend/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -16,3 +16,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- WEB/module_10/backend/users/views.py
+++ WEB/module_10/backend/users/views.py
@@ -1,23 +1,52 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from .forms import LoginForm
+from .forms import RegisterForm
+
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, "users/register.html", context={"author": "none"})
+    if request.user.is_authenticated:
+        return redirect(to='test_app:index')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='users:dashboard')
+        else:
+            return render(request, 'users/register.html', context={"form": form})
+
+    return render(request, 'users/register.html', context={"form": RegisterForm()})
 
 
-def login(request):
-    return render(request, "users/login.html", context={"author": "none"})
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect(to='test_app:index')
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+        login(request, user)
+        return redirect(to='test_app:index')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
 
 
+@login_required()
 def dashboard(request):
     return render(request, "users/dashboard.html", context={"author": "nones"})
 
 
-def sign_out():
-    # logout logic
-    return redirect(to="/")
+@login_required()
+def sign_out(request):
+    logout(request)
+    return redirect(to="test_app:index")
 
 
 def add_quote(request):
